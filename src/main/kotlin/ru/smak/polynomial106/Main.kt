eee680d728d0cac478829c1b52e2ae0da050f301
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-13 13:53:16 +0300

    Полином от 13.09.2023 (1 пара)

--- src/main/kotlin/ru/smak/polynomial106/Main.kt
+++ src/main/kotlin/ru/smak/polynomial106/Main.kt
@@ -1,2 +1,13 @@
 package ru.smak.polynomial106
 
+import ru.smak.polynomial106.math.Polynomial
+
+var t: Polynomial? = null
+fun main() {
+    val p1 = Polynomial(mapOf(1 to 5.0, 2 to -3.0, 5 to 1.0))
+    val p2 = Polynomial(0.0, 0.0, 7.0, -4.0)
+    println(p1)
+    println(p2)
+    println(p1.times(0.0))
+    println(p1.plus(p2))
+}
--- src/main/kotlin/ru/smak/polynomial106/math/Polynomial.kt
+++ src/main/kotlin/ru/smak/polynomial106/math/Polynomial.kt
@@ -1,4 +1,31 @@
 package ru.smak.polynomial106.math
 
-class Polynomial {
+class Polynomial(coeffs: Map<Int, Double>) {
+    private val _coeffs: MutableMap<Int, Double>
+    val coeffs: Map<Int, Double>
+        get() = _coeffs.toMap()
+
+    init{
+        _coeffs = coeffs.toMutableMap()
+    }
+
+    constructor(vararg coeffs: Double) : this(HashMap<Int, Double>().apply {
+        coeffs.forEachIndexed{
+            i,v -> if (v!=0.0) put(i, v)
+        }
+    })
+
+    fun plus (other: Polynomial) = Polynomial(_coeffs.toMutableMap().also {
+        other._coeffs.forEach { (k, v) -> it[k]= v + (it[k] ?: 0.0) }
+    })
+
+    fun times(k: Double) = Polynomial(List(_coeffs.size){
+        _coeffs.keys.elementAt(it) to _coeffs[_coeffs.keys.elementAt(it)]!! * k
+    }.toMap())
+
+
+    override fun toString(): String {
+        return _coeffs.toList().joinToString { "${it.first}: ${it.second}" }
+    }
+
 }
