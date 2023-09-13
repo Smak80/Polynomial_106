be578427f066e0634742fd34176af76034d5fba1
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-13 15:42:36 +0300

    Полином от 13.09.2023 (2 пара)

--- src/main/kotlin/ru/smak/polynomial106/Main.kt
+++ src/main/kotlin/ru/smak/polynomial106/Main.kt
@@ -4,10 +4,11 @@ import ru.smak.polynomial106.math.Polynomial
 
 var t: Polynomial? = null
 fun main() {
-    val p1 = Polynomial(mapOf(1 to 5.0, 2 to -3.0, 5 to 1.0))
-    val p2 = Polynomial(0.0, 0.0, 7.0, -4.0)
+    val p1 = Polynomial(mapOf(1 to -1.0, 2 to -3.0, 5 to 1.0))
+    val p2 = Polynomial(1.0, 1.0, 3.0, -4.0)
     println(p1)
     println(p2)
-    println(p1.times(0.0))
-    println(p1.plus(p2))
+    println(p1*0.0)
+    println(p1 + p2)
+    println(p1*p2)
 }
--- src/main/kotlin/ru/smak/polynomial106/math/Polynomial.kt
+++ src/main/kotlin/ru/smak/polynomial106/math/Polynomial.kt
@@ -1,31 +1,51 @@
 package ru.smak.polynomial106.math
 
+import kotlin.math.abs
+
 class Polynomial(coeffs: Map<Int, Double>) {
     private val _coeffs: MutableMap<Int, Double>
     val coeffs: Map<Int, Double>
         get() = _coeffs.toMap()
 
     init{
-        _coeffs = coeffs.toMutableMap()
+        _coeffs = coeffs.filter { (k,v) -> v != 0.0 }.toSortedMap()
+        if(_coeffs.isEmpty()) _coeffs[0] = 0.0
     }
 
-    constructor(vararg coeffs: Double) : this(HashMap<Int, Double>().apply {
-        coeffs.forEachIndexed{
-            i,v -> if (v!=0.0) put(i, v)
-        }
-    })
+    constructor(vararg coeffs: Double) : this(coeffs.mapIndexed{ i, v -> i to v }.toMap())
 
-    fun plus (other: Polynomial) = Polynomial(_coeffs.toMutableMap().also {
+    operator fun plus (other: Polynomial) = Polynomial(_coeffs.toMutableMap().also {
         other._coeffs.forEach { (k, v) -> it[k]= v + (it[k] ?: 0.0) }
     })
 
-    fun times(k: Double) = Polynomial(List(_coeffs.size){
+    operator fun minus (other: Polynomial) = Polynomial(_coeffs.toMutableMap().also {
+        other._coeffs.forEach { (k, v) -> it[k]= v - (it[k] ?: 0.0) }
+    })
+
+    operator fun times(k: Double) = Polynomial(List(_coeffs.size){
         _coeffs.keys.elementAt(it) to _coeffs[_coeffs.keys.elementAt(it)]!! * k
     }.toMap())
 
+    operator fun times(other: Polynomial) = Polynomial(mutableMapOf<Int, Double>().also{ c ->
+        _coeffs.forEach{ (k1 , v1) ->
+            other._coeffs.forEach { (k2, v2) ->
+                c[k1+k2] = v1*v2+(c[k1+k2]?:0.0)
+            }
+        }
+    })
 
-    override fun toString(): String {
-        return _coeffs.toList().joinToString { "${it.first}: ${it.second}" }
-    }
+    override fun toString() = toString("x")
+ //       return _coeffs.toList().joinToString { "${it.first}: ${it.second}" }
+
+
+    fun toString(variable: String) = _coeffs.toList().reversed().map{ (k, v) ->
+        StringBuilder().apply {
+            append(if (v >= 0.0) if (k != _coeffs.keys.max()) "+" else "" else "-")
+            if(abs(v) != 1.0 || k == 0) append(abs(v))
+            if(k != 0) append(variable)
+            if(k > 1) append("^$k")
+        }.toString()
+
+    }.joinToString("")
 
 }
