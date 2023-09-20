6b60812abb65adccdc495e2136f243675d9542b2
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-20 09:56:05 +0300

    Полином от 20.09.2023 (1 пара)

--- src/main/kotlin/ru/smak/polynomial106/Main.kt
+++ src/main/kotlin/ru/smak/polynomial106/Main.kt
@@ -1,6 +1,7 @@
 package ru.smak.polynomial106
 
 import ru.smak.polynomial106.math.Polynomial
+import ru.smak.polynomial106.math.eq
 
 var t: Polynomial? = null
 fun main() {
@@ -11,4 +12,13 @@ fun main() {
     println(p1*0.0)
     println(p1 + p2)
     println(p1*p2)
+
+    val p7 = Polynomial(4.0, 1.0, 1.0)
+    println(p7(2.0))
+
+    println(0.6+0.1)
+    println(0.8-0.1)
+    println(0.6+0.1 == 0.8-0.1)
+    println((6e-20+2e-20) eq (8e-20-1e-20))
+    println(Math.ulp(0.0))
 }
--- src/main/kotlin/ru/smak/polynomial106/math/Polynomial.kt
+++ src/main/kotlin/ru/smak/polynomial106/math/Polynomial.kt
@@ -1,6 +1,8 @@
 package ru.smak.polynomial106.math
 
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.pow
 
 class Polynomial(coeffs: Map<Int, Double>) {
     private val _coeffs: MutableMap<Int, Double>
@@ -8,7 +10,7 @@ class Polynomial(coeffs: Map<Int, Double>) {
         get() = _coeffs.toMap()
 
     init{
-        _coeffs = coeffs.filter { (k,v) -> v != 0.0 }.toSortedMap()
+        _coeffs = coeffs.filter { (k,v) -> v neq 0.0 }.toSortedMap()
         if(_coeffs.isEmpty()) _coeffs[0] = 0.0
     }
 
@@ -34,18 +36,35 @@ class Polynomial(coeffs: Map<Int, Double>) {
         }
     })
 
-    override fun toString() = toString("x")
- //       return _coeffs.toList().joinToString { "${it.first}: ${it.second}" }
+    operator fun div(scalar: Double) = Polynomial(_coeffs.map { (k, v) -> if (v eq 0.0) throw ArithmeticException("Division by zero") else k to v / scalar }.toMap())
 
+    operator fun invoke(x: Double) = _coeffs.toList().fold(0.0) {
+            result, value ->
+        result + x.pow(value.first) * value.second
+    }
+
+    override fun toString() = toString("x")
 
     fun toString(variable: String) = _coeffs.toList().reversed().map{ (k, v) ->
         StringBuilder().apply {
-            append(if (v >= 0.0) if (k != _coeffs.keys.max()) "+" else "" else "-")
-            if(abs(v) != 1.0 || k == 0) append(abs(v))
-            if(k != 0) append(variable)
-            if(k > 1) append("^$k")
+            if (v.neq(0.0, 1e-12)) {
+                append(if (v > 0.0 || v.eq(0.0, 1e-12)) if (k != _coeffs.keys.max()) "+" else "" else "-")
+                if (abs(v) neq 1.0 || k == 0) append(abs(v))
+                if (k != 0) append(variable)
+                if (k > 1) append("^$k")
+            }
         }.toString()
 
     }.joinToString("")
 
 }
+
+fun Double.eq(other: Double, eps: Double = 1e-15) =
+    abs(this - other) < eps
+
+infix fun Double.eq(other: Double) =
+    abs(this - other) < max(Math.ulp(this), Math.ulp(other)) * 2
+
+fun Double.neq(other: Double, eps: Double = 1e-15) = !(eq(other, eps))
+
+infix fun Double.neq(other: Double) = !(this eq other)
