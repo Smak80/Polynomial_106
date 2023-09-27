d46f63b66aa1997ca28ef5dc51cb9d54f129aae3
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-27 09:14:00 +0300

    Полином от 27.09.2023 (1 пара)

--- src/main/kotlin/ru/smak/polynomial106/math/polynomial/LagrangePoly.kt
+++ src/main/kotlin/ru/smak/polynomial106/math/polynomial/LagrangePoly.kt
@@ -20,7 +20,7 @@ class LagrangePoly(points: Map<Double, Double>) : Polynomial() {
     }
 
     private fun createFundamental(xk: Double) = Polynomial(1.0).also {
-        _points.forEach { (xi, y) -> if (xi neq xk) it *= Polynomial(-xi, 1.0) / (xk - xi) }
+        _points.forEach { (xi, _) -> if (xi neq xk) it *= Polynomial(-xi, 1.0) / (xk - xi) }
     }
 
 }
